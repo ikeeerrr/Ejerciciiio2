57f334ab8167599fe7f3e65e39b4773c0a88d9d8
iker <sanchezgalaniker2005@gmail.com>
2024-06-05 13:29:22 +0200

    Subir todo

--- src/AnalisisEdad.java
+++ src/AnalisisEdad.java
@@ -1,9 +1,36 @@
-
+import java.util.*;
+/*
+ * Clase donde analizaremos el método
+ */
 public class AnalisisEdad {
+/*
+ * Método principal donde generaremos las edades
+ */
+	Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+	private ArrayList<Integer> edades;
+	int edad;
+
+	
+	public AnalisisEdad(ArrayList<Integer> edades ) {
+		this.edades = edades;
 	}
-
+	public String comprobar(ArrayList<Integer> edades) {
+		int menores = 0;
+		int mayores = 0;
+		int jubilados = 0;
+		
+		for(int i=0;i<edades.size();i++) {
+			if(edades.get(i)<18) {
+				menores = menores + 1;
+			}else if (edades.get(i)>=18 && edades.get(i)<65) {
+				mayores = mayores + 1;
+			}else if (edades.get(i)>65) {
+				jubilados = jubilados + 1;
+			}
+			
+		}
+		String mensaje = " menores de edad " + menores + " mayores de edad " + mayores + " jubilados " + jubilados;
+		return mensaje;
+	}
 }
---
+++ src/PrincipalApp.java
@@ -0,0 +1,15 @@
+import java.util.*;
+/*
+ * Clase para analizar las edades de la población introducidas
+ */
+public class PrincipalApp {
+/*
+ * Constructor que introducirá el array con las edades
+ */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer>ArrayEdades = new ArrayList();
+	}
+
+}
